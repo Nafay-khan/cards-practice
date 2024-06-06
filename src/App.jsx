480d3c6156db8d411e2e1b64e019ba8ce4158657
Nafay-Khan <nafaykhan422@mail.com>
2024-06-06 19:24:48 +0500

    list added

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from './assets/components/Navbar'
+import Card from './assets/components/Card'
+import axios from 'axios'
+
+function App() {
+    const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    axios('https://dummyjson.com/products')
+      .then((res) => {
+        console.log(res.data.products)
+        setProducts(res.data.products)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [])
+  return (
+     <>
+     <Navbar/>
+      <div className='flex justify-center gap-5 flex-wrap mt-[2rem] '>
+      {products ? 
+      products.map((item)=>{
+        return <Card key={item.id} title={item.title} description={item.description} img={item.thumbnail} price={item.price} />
+      })
+      : <h1><span className="loading loading-spinner loading-xl"></span></h1>}
+      </div>
+     </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Card(props) {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure><img src={props.img} alt="Shoes" /></figure>
+      <div className="card-body">
+          <h2 className="card-title">{props.title}</h2>
+          <p>{props.description.slice(0 , 50)}...</p>
+          <p className="text-2xl">${props.price}</p>
+          <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+          </div>
+      </div>
+  </div>
+  )
+}
+
+export default Card
