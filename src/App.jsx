0cd3dabfa1804a249f47ec703419710e64a86d4b
Nafay-khan <157211666+Nafay-khan@users.noreply.github.com>
2024-12-30 18:37:50 +0500

    add comments in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Navbar from './assets/components/Navbar'
 import Card from './assets/components/Card'
 import axios from 'axios'
-
+//react app for cards props 
 function App() {
     const [products, setProducts] = useState(null);
 
